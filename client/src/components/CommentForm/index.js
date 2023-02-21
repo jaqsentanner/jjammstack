2ba3b26dfc8869515c25b122d2e38f4cc417160a
Jeff Fleischman <FleischmanJ88@gmail.com>
2023-02-20 18:47:06 -0600

    fixed misc button issues

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -52,9 +52,14 @@ const CommentForm = ({ postId }) => {
           onChange={handleChange}
         ></textarea>
 
-        <button className="btn col-12 col-md-3" type="submit">
-          Submit
-        </button>
+        <button className="btn col-12 col-md-3" type="submit"
+            ><a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+            <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+            <span className="relative px-20 py-7 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+            <span className="relative text-white">Submit Post</span>
+            </span>
+            </a>
+          </button>
       </form>
 
       {error && <div>Something went wrong...</div>}
