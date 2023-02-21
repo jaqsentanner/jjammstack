2ec56423784b5dfcfc4909b3e6fcdf913917e454
fridayknight13 <73671454+madams1972@users.noreply.github.com>
2023-02-21 15:05:38 -0600

    fixed errors in the console

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { useMutation } from '@apollo/client';
 import { ADD_COMMENT } from '../../utils/mutations';
@@ -54,28 +55,28 @@ const CommentForm = ({ postId }) => {
         ></textarea>
 
         <button className="btn col-12 col-md-3" type="submit"
-            ><a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+            ><Link href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
             <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
             <span className="relative px-20 py-7 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
             <span className="relative text-white">Submit Post</span>
             </span>
-            </a>
+            </Link>
           </button>
           <button className="btn col-12 col-md-3" type="submit"
-            ><a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+            ><Link href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
             <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
             <span className="relative px-10 py-flex transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
             <span className="relative text-white">🖉 Edit</span>
             </span>
-            </a>
+            </Link>
           </button>
           <button className="btn col-12 col-md-3" type="submit"
-            ><a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+            ><Link href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
             <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
             <span className="relative px-7 py-flex transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
             <span className="relative text-white">🗑 Delete</span>
             </span>
-            </a>
+            </Link>
           </button>
       </form>
 
